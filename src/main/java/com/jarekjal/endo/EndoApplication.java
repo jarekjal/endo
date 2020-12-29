3c8a06aaca126954e148745490dc2da3e65340c4
jarekjal <jarekjal@wp.pl>
2020-12-29 11:26:00 +0100

    initial commit

---
+++ src/main/java/com/jarekjal/endo/EndoApplication.java
@@ -0,0 +1,13 @@
+package com.jarekjal.endo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarekjal/endo/EndoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarekjal.endo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
