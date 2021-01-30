fa345b08cef706f61cae419ee298d2e907216725
jarekjal <jarekjal@wp.pl>
2021-01-30 12:52:03 +0100

    brakujaca adnotacja @Repository (dzialalo tez bez chyba)

--- src/main/java/com/jarekjal/endo/repo/TrainingRepo.java
+++ src/main/java/com/jarekjal/endo/repo/TrainingRepo.java
@@ -2,9 +2,11 @@ package com.jarekjal.endo.repo;
 
 import com.jarekjal.endo.repo.entities.Training;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TrainingRepo extends CrudRepository<Training, Long> {
 
     List<Training> findByUserName(String username);
